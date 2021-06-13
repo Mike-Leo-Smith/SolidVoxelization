aa2c1bb69c8ade743123d97a6e5414fe3b71f58d
mike <mango@live.cn>
2021-06-13 20:35:59 +0800

    added cmake root

---
+++ src/core/camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mike Smith on 2021/6/13.
+//
+
+#include "camera.h"
---
+++ src/core/camera.h
@@ -0,0 +1,10 @@
+//
+// Created by Mike Smith on 2021/6/13.
+//
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+class Camera {
+};
