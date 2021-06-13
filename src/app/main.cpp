9ff68c8ee730369956edbcbe1e1390b13a2e0686
mike <mango@live.cn>
2021-06-14 01:56:17 +0800

    clean up

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,9 +1,3 @@
-// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
-// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
-// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
-// Read online: https://github.com/ocornut/imgui/tree/master/docs
-
-#include <iostream>
 #include <random>
 #include <vector>
 
